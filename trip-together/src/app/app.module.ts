dfa69f70b5cb9b4622b59b3348d4da1968b180a1
Todor Borisov <todor0203@gmail.com>
2023-07-06 03:43:40 +0300

    Add main component and home, add icons to the buttons in the navbar

--- trip-together/src/app/app.module.ts
+++ trip-together/src/app/app.module.ts
@@ -5,10 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from './core/core.module';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    MainComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
