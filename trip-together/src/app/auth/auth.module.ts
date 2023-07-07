0d4df14e2f06dc84ec82f59b4f8dfb355cc9401f
Todor Borisov <todor0203@gmail.com>
2023-07-07 12:08:02 +0300

    Add some components but not attached

---
+++ trip-together/src/app/auth/auth.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
+import { RegisterComponent } from './register/register.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ProfileComponent } from './profile/profile.component';
+
+
+
+@NgModule({
+  declarations: [
+    LoginComponent,
+    LogoutComponent,
+    RegisterComponent,
+    PageNotFoundComponent,
+    ProfileComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class AuthModule { }
