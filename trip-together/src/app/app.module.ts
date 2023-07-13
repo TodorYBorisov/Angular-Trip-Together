f803d0a793bca70b9755824a3fc8290b5a658ed5
Todor Borisov <todor0203@gmail.com>
2023-07-13 17:13:41 +0300

    Add shared module with louder component to the project.

--- trip-together/src/app/app.module.ts
+++ trip-together/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from './core/core.module';
 import { FeaturesModule } from './features/features.module';
 import { AuthModule } from './auth/auth.module';
+import { SharedModule } from './shared/shared.module';
 
 
 
@@ -21,6 +22,7 @@ import { AuthModule } from './auth/auth.module';
     BrowserAnimationsModule,
     CoreModule,
     FeaturesModule,
+    SharedModule,
     AppRoutingModule,
     
   ],
