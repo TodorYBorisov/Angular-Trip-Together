968236ede25b563b6f38f5ad85f485fe18128e9e
Todor Borisov <todor0203@gmail.com>
2023-07-20 01:19:36 +0300

    Added error validation to register fields

--- trip-together/src/app/auth/register/register.component.ts
+++ trip-together/src/app/auth/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
+import { FormBuilder, Validators } from '@angular/forms';
+import { matchPasswordsValidator } from 'src/app/shared/validators/match-password-validator';
 
 @Component({
   selector: 'app-register',
@@ -7,4 +11,27 @@ import { Component } from '@angular/core';
 })
 export class RegisterComponent {
 
+  form = this.formBuilder.group({
+    username: ['', [Validators.required, Validators.minLength(5)]],
+    email: ['', [Validators.required, Validators.email]],
+    tel: ['', [Validators.required]],
+    gender: ['', [Validators.required]],
+    passGroup: this.formBuilder.group({
+      password: ['', [Validators.required, Validators.minLength(5)]],
+      rePassword: ['', [Validators.required]],
+    }, {
+      validators: [matchPasswordsValidator('password', 'rePassword')]
+    })
+
+  })
+
+
+  constructor(private formBuilder: FormBuilder, private authService: AuthService, private router: Router) { }
+
+  registerHandler(): void {
+    if (this.form.invalid) { return; }
+
+    console.log(this.form.value);
+    // this.form.reset();
+  }
 }
