3a7c57ed2f2fb9ce34f4c461d6ffb24cb086e491
Todor Borisov <todor0203@gmail.com>
2023-07-20 14:01:48 +0300

    Created add form with added all error validations to the form for each field

--- trip-together/src/app/features/create/create.component.ts
+++ trip-together/src/app/features/create/create.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { TripService } from '../trip.service';
+import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-create',
@@ -6,5 +9,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./create.component.css']
 })
 export class CreateComponent {
+
+
+  constructor(private tripService: TripService, private router: Router) { }
+
+  onSubmit(form: NgForm) {
+    if (form.invalid) { return; }
+
+    const value: { startPoint: string, endPoint: string, date: string, time: string, imageUrl: string, brand: string, seats: number, price: number, description: string } = form.value;
+
+
+    console.log(form.value);
+    
+  }
 
 }
