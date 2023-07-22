e54e3317e6459663d6e9d5a8b2234e4556eb51bc
Todor Borisov <todor0203@gmail.com>
2023-07-23 01:29:05 +0300

    Renamed to interfaces

---
+++ trip-together/src/app/shared/interfaces/trip.ts
@@ -0,0 +1,17 @@
+import { User } from "./user";
+
+export interface Trip {
+    _id: string,
+    startPoint: string,
+    endPoint: string,
+    date: string,
+    time: string,
+    imageUrl: string,
+    brand: string,
+    seats: number,
+    price: number,
+    description: string,
+    userId: User,
+    buddies: string[],
+    __v: number
+}
---
+++ trip-together/src/app/shared/interfaces/user.ts
@@ -0,0 +1,11 @@
+export interface User {
+  _id: string
+  username: string,
+  email: string,
+  tel: string,
+  gender: string
+  password: string,
+  trips: string[];
+  created_at: string,
+  __v: number,
+}
