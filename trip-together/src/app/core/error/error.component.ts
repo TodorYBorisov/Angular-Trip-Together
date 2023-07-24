3daa86e52d89db38602088b3720c13fcbebf4c86
Todor Borisov <todor0203@gmail.com>
2023-07-24 13:32:44 +0300

    Update error.component.ts

--- trip-together/src/app/core/error/error.component.ts
+++ trip-together/src/app/core/error/error.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ErrorService } from './error.service';
+
+
 
 @Component({
   selector: 'app-error',
   templateUrl: './error.component.html',
   styleUrls: ['./error.component.css']
 })
-export class ErrorComponent {
+export class ErrorComponent implements OnInit {
+
+  apiError$ = this.errorService.apiError$$.asObservable();
+  errorMsg = '';
+
+  constructor(private errorService: ErrorService) { }
+
+  ngOnInit(): void {
+    this.apiError$.subscribe((err: any) => {
+      this.errorMsg = err.message;
+    });
+
+  }
+  
+
 
 }
