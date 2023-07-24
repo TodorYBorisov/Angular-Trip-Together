9202ff7d03b695048a4eaa55d72f9d57e009a837
Todor Borisov <todor0203@gmail.com>
2023-07-24 13:32:52 +0300

    Create app.interceptor.ts

---
+++ trip-together/src/app/app.interceptor.ts
@@ -0,0 +1,40 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HTTP_INTERCEPTORS } from "@angular/common/http";
+import { Injectable, Provider } from "@angular/core";
+import { Router } from "@angular/router";
+import { Observable, catchError } from "rxjs";
+import { environment } from "src/environments/environment";
+import { ErrorService } from "./core/error/error.service";
+
+
+const apiUrl = environment.apiUrl;
+
+@Injectable()
+export class AppInterceptor implements HttpInterceptor {
+
+    constructor(private router: Router, private errorService: ErrorService) { }
+
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        if (req.url.startsWith('/api')) {
+            req = req.clone({ url: req.url.replace('/api', apiUrl), withCredentials: true }); //Cookie-->JWT
+        }
+
+        return next.handle(req).pipe(
+            catchError((err) => {
+                if (err.status === 401) {
+                    this.router.navigate(['/auth/login']);
+                } else {
+                    this.errorService.setError(err);
+                    this.router.navigate(['/error']);
+                }
+
+                return [err];
+            })
+        );
+    }
+}
+
+export const appInterceptorProvider: Provider = {
+    multi: true,
+    useClass: AppInterceptor,
+    provide: HTTP_INTERCEPTORS
+}
