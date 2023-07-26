eb9b8b09df1b5fa6525551f0a5d8ca05adb68eef
Todor Borisov <todor0203@gmail.com>
2023-07-26 11:43:01 +0300

    Added some functionality to profile page

--- trip-together/src/app/auth/profile/profile.component.ts
+++ trip-together/src/app/auth/profile/profile.component.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from '../auth.service';
+import { User } from 'src/app/shared/interfaces/user';
+import { TripService } from 'src/app/features/trip.service';
+import { Trip } from 'src/app/shared/interfaces/trip';
 
 interface Profile { //тук си правим един интерфей на обекта
   username: string,
@@ -7,22 +11,27 @@ interface Profile { //тук си правим един интерфей на о
   tel: string
 }
 
+
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
 
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+
+  user: User | undefined;
+  tripsFound: Trip[] = [];
+  
 
   isEditMode: boolean = false;
   ifTrips: boolean = false
 
   //правим си един профил детаилс
   profileDetails: Profile = {
-    username: 'toshko',
-    email: 'toshko@gmail.com',
-    tel: '885500500'
+    username: '',
+    email: '',
+    tel: '',
   };
 
   form = this.formBuilder.group({ //правим форма група с валидациите
@@ -32,7 +41,39 @@ export class ProfileComponent {
 
   })
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder, private authService: AuthService, private tripService: TripService) { }
+
+  ngOnInit(): void {
+
+    if (this.authService.user) {
+      const { username, email, tel } = this.authService.user;
+      this.profileDetails = { username, email, tel };
+
+      this.form.setValue({
+        username,
+        email,
+        tel,
+      });
+    }
+
+    //const { username, email, tel } = this.authService.user! //!така гарантираме, че ще имаме потребител
+    this.geAlltUserTrips();
+  }
+  
+
+  get gender(): string {
+    return this.authService.user?.gender || '';
+
+  }
+
+  // get trips(): any {
+  //   return this.authService.user?.trips.length
+  // }
+
+get userId():any{
+  return this.authService.user?._id
+}
+
 
   toggleEditMode(): void {
     this.isEditMode = !this.isEditMode;
@@ -44,8 +85,19 @@ export class ProfileComponent {
     //задаваме си променлива да е равна на данните ор формата и те да са от типа на профила
     this.profileDetails = { ...this.form.value } as Profile;
 
-    console.log(this.form.value);
-    this.toggleEditMode()
+    const { username, email, tel } = this.profileDetails;
+    this.authService.updateProfile(username, email, tel).subscribe(() => {
+
+      this.toggleEditMode()
+    })
+
   }
 
+  geAlltUserTrips():void {
+    this.tripService.getUserTrips(this.userId).subscribe((trips: Trip[]) => {
+      this.tripsFound = trips;
+      console.log(this.tripsFound);
+    },
+    );
+  }
 }
