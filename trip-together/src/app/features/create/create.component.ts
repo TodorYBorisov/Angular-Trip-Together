2dec3d4b1ee844e07770677dfbf446e03bcbf6be
Todor Borisov <todor0203@gmail.com>
2023-07-26 11:42:39 +0300

    Update some minor changes

--- trip-together/src/app/features/create/create.component.ts
+++ trip-together/src/app/features/create/create.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { TripService } from '../trip.service';
 import { Router } from '@angular/router';
 import { NgForm } from '@angular/forms';
+import { Trip } from 'src/app/shared/interfaces/trip';
+import { AuthService } from 'src/app/auth/auth.service';
+import { User } from 'src/app/shared/interfaces/user';
 
 @Component({
   selector: 'app-create',
@@ -10,17 +13,22 @@ import { NgForm } from '@angular/forms';
 })
 export class CreateComponent {
 
-
-  constructor(private tripService: TripService, private router: Router) { }
+  constructor(private tripService: TripService, private router: Router, private authService: AuthService) { }
 
   onSubmit(form: NgForm) {
     if (form.invalid) { return; }
 
-    const value: { startPoint: string, endPoint: string, date: string, time: string, imageUrl: string, brand: string, seats: number, price: number, description: string } = form.value;
-
-
+    //const value: { startPoint: string, endPoint: string, date: string, time: string, imageUrl: string, brand: string, seats: number, price: number, description: string } = form.value;
+    const { startPoint, endPoint, date, time, imageUrl, brand, seats, price, description } = form.value;
     console.log(form.value);
-    
+
+    const _id = this.authService.user!._id;
+    const newTrip: Trip = {...form.value, _id};
+
+    this.tripService.createTrip(newTrip).subscribe(() => {
+      this.router.navigate(['/trip/catalog'])
+    });
+
   }
 
 }
