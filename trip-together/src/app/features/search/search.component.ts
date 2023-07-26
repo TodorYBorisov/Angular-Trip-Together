f1e6ba273cc0bc18cf6e3813398e934e2443b8aa
Todor Borisov <todor0203@gmail.com>
2023-07-26 11:05:14 +0300

    Search functionality added to project, only sing in users will be able to use search

--- trip-together/src/app/features/search/search.component.ts
+++ trip-together/src/app/features/search/search.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Trip } from 'src/app/shared/interfaces/trip';
+import { TripService } from '../trip.service';
+import { AuthService } from 'src/app/auth/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-search',
@@ -6,5 +10,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent {
+  tripsFound!: Trip[];
+  query: string = '';
+  subscription$!: Subscription;
+  errorMsgFromServer!: string;
+  hasResult: boolean = false;
+
+  constructor(private tripService: TripService, private authService: AuthService) { }
+
+  get isLogged(): boolean {
+    return this.authService.isLogged
+  }
+
+  onSearch(searchStr: string): void {
+    this.query = searchStr;
+    this.subscription$ = this.tripService.search(this.query).subscribe({
+      next: (data) => {
+        this.tripsFound = data;
+        this.hasResult = this.tripsFound.length != 0;
+      },
+      error: (error) => this.errorMsgFromServer = error.error.message,
+      complete: () => this.subscription$.unsubscribe(),
+    })
+
+  }
+
 
 }
