6ac732e4fec30a757ea4e9e614291e001ef82c04
Todor Borisov <todor0203@gmail.com>
2023-07-27 23:45:02 +0300

    Update app.interceptor.ts

--- trip-together/src/app/app.interceptor.ts
+++ trip-together/src/app/app.interceptor.ts
@@ -10,12 +10,18 @@ const { apiUrl } = environment;
 
 @Injectable()
 export class AppInterceptor implements HttpInterceptor {
+    constructor(private router: Router, private errorServie: ErrorService) { }
 
-    constructor(private router: Router, private errorService: ErrorService) { }
-
-    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    intercept(
+        req: HttpRequest<any>,
+        next: HttpHandler
+    ): Observable<HttpEvent<any>> {
         if (req.url.startsWith('/api')) {
-            req = req.clone({ url: req.url.replace('/api', apiUrl), withCredentials: true }); //Cookie-->JWT
+            req = req.clone({
+                url: req.url.replace('/api', apiUrl),
+                withCredentials: true, // Cookie -> JWT
+               
+            });
         }
 
         return next.handle(req).pipe(
@@ -23,7 +29,7 @@ export class AppInterceptor implements HttpInterceptor {
                 if (err.status === 401) {
                     this.router.navigate(['/auth/login']);
                 } else {
-                    this.errorService.setError(err);
+                    this.errorServie.setError(err);
                     this.router.navigate(['/error']);
                 }
 
