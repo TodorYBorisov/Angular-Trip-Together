2266bec968689872039c7fb2de26fbef7926ab93
Todor Borisov <todor0203@gmail.com>
2023-08-01 19:09:04 +0300

    Added dynamic carousel with users to about page

--- trip-together/src/app/about/about.component.ts
+++ trip-together/src/app/about/about.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  styleUrls: ['./about.component.css'],
+ 
 })
 export class AboutComponent {
 
+  
 }
