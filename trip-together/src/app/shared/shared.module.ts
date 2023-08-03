062fbe8fec5b39a62bc6202d1365d6b1fc5a26b7
Todor Borisov <todor0203@gmail.com>
2023-08-03 18:14:26 +0300

    Update shared.module.ts

--- trip-together/src/app/shared/shared.module.ts
+++ trip-together/src/app/shared/shared.module.ts
@@ -2,17 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoaderComponent } from './loader/loader.component';
 import { ElapsedTimePipe } from './pipes/elapsed-time.pipe';
+import { TempConvertorPipe } from './pipes/temp-convertor.pipe';
 
 
 @NgModule({
   declarations: [
     LoaderComponent,
     ElapsedTimePipe,
+    TempConvertorPipe,
 
   ],
   imports: [
     CommonModule,
   ],
-  exports: [LoaderComponent, ElapsedTimePipe]
+  exports: [
+    LoaderComponent,
+    ElapsedTimePipe,
+    TempConvertorPipe
+  ]
 })
 export class SharedModule { }
