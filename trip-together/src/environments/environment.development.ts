c230a62dcfdd37cddd6fcce6adbf9eda596cd67f
Todor Borisov <todor0203@gmail.com>
2023-08-07 20:44:13 +0300

    Update environment.development.ts

--- trip-together/src/environments/environment.development.ts
+++ trip-together/src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
     // production: true,
     //apiUrl: 'http://localhost:3000/api'
-   apiUrl: 'https://server1-8jki.onrender.com'
+   apiUrl: 'https://server1-8jki.onrender.com/api'
 };
