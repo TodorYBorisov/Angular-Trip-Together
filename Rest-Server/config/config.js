b6893b63b6cff7811fce5bf1385858d08bf73df5
Todor Borisov <todor0203@gmail.com>
2023-08-07 17:36:37 +0300

    Update config.js

--- Rest-Server/config/config.js
+++ Rest-Server/config/config.js
@@ -3,7 +3,7 @@ const env = process.env.NODE_ENV || 'development';
 const config = {
     development: {
         port: process.env.PORT || 3000,
-        dbURL: 'mongodb+srv://toshko:toshko12345@cluster0.v7dgwod.mongodb.net/?retryWrites=true&w=majority',
+        dbURL: 'mongodb://127.0.0.1:27017/TripTogether',
         origin: ['http://localhost:5555', 'http://localhost:4200']
     },
     production: {
@@ -15,5 +15,5 @@ const config = {
 
 module.exports = config[env];
 
-//mongoAtlas -- mongodb+srv://toshko:<password>@cluster0.v7dgwod.mongodb.net/?retryWrites=true&w=majority
+//mongoAtlas -- mongodb+srv://toshko:toshko12345@cluster0.v7dgwod.mongodb.net/?retryWrites=true&w=majority
 // mongoCompass -- mongodb://127.0.0.1:27017/TripTogether
