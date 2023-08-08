91751e806ee7b111d79da56d0d420ad25278ab85
Todor Borisov <todor0203@gmail.com>
2023-08-08 19:56:49 +0300

    Update index.js

--- Rest-Server/index.js
+++ Rest-Server/index.js
@@ -27,3 +27,17 @@ dbConnector()
   })
   .catch(console.error);
   
+
+//not tested
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', config.origin);
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+//   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//   res.setHeader('Access-Control-Allow-Credentials', 'true');
+
+//   if (req.method === 'OPTIONS') {
+//     res.sendStatus(200);
+//   } else {
+//     next();
+//   }
+// });
