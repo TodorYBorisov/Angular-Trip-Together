1c2021a467afc80b9baf45e5cd9d9b62d8cdcada
Todor Borisov <todor0203@gmail.com>
2023-08-10 18:51:04 +0300

    Fixed a typo and updated some documentation files.

--- trip-together/src/app/auth/auth-routing.module.ts
+++ trip-together/src/app/auth/auth-routing.module.ts
@@ -18,7 +18,7 @@ const routes: Routes = [
             {
                 path: 'register',
                 component: RegisterComponent,
-                title: 'Sing Up',
+                title: 'Sign Up',
             },
             {
                 path: 'logout',
