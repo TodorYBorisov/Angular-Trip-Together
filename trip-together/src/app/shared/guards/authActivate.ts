a266cf095a8f2f1b8980f26a89d5539cbdebad7b
Todor Borisov <todor0203@gmail.com>
2023-08-11 22:13:30 +0300

    Update authActivate.ts

--- trip-together/src/app/shared/guards/authActivate.ts
+++ trip-together/src/app/shared/guards/authActivate.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@angular/core";
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from "@angular/router";
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
 import { Observable } from "rxjs";
 import { AuthService } from "src/app/auth/auth.service";
 
@@ -8,7 +8,7 @@ import { AuthService } from "src/app/auth/auth.service";
 
 export class AuthActivate implements CanActivate {
 
-    constructor(private authService: AuthService) { }
+    constructor(private authService: AuthService, private router: Router) { }
 
     canActivate(
         route: ActivatedRouteSnapshot,
@@ -17,6 +17,10 @@ export class AuthActivate implements CanActivate {
         | UrlTree
         | Observable<boolean | UrlTree>
         | Promise<boolean | UrlTree> {
-        return this.authService.isLogged;
+            if(this.authService.isLogged){
+                return true;
+             }
+             return this.router.createUrlTree(['/auth/login'])
+        //return this.authService.isLogged;
     }
 }
